38367ce41cd68303566eea5f3397543cb2fa8d86
chowy1026 <chowy1026@gmail.com>
2017-03-31 09:47:07 +0800

    Modifications to run with Python 3.6

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,7 +44,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
@@ -59,7 +59,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             email.close()
 
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
@@ -71,5 +71,3 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
