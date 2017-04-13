116d021756f896d279c6271c6a81f146f6d2a9fa
chowy1026 <chowy1026@gmail.com>
2017-04-13 22:46:04 +0800

    K-Means clustering code

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -8,6 +8,7 @@
 
 
 import pickle
+import pprint
 import numpy
 import matplotlib.pyplot as plt
 import sys
@@ -39,7 +40,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 ### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
@@ -51,26 +52,59 @@ feature_2 = "exercised_stock_options"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
+data2 = featureFormat(data_dict, features_list, remove_NaN=True )
 poi, finance_features = targetFeatureSplit( data )
+poi2, finance_features2 = targetFeatureSplit( data2 )
 
 
+lst_stock_val = []
+pprint.pprint(data_dict)
+for k, v in data_dict.items():
+    stats = v
+    pprint.pprint(stats)
+    for key, val in stats.items():
+        if key == "salary" and val != 'NaN':
+            lst_stock_val.append(val)
+
+print(lst_stock_val)
+print(max(lst_stock_val))
+print(min(lst_stock_val))
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
+
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+clf2 = KMeans(n_clusters=2)
+pred2 = clf2.fit_predict( finance_features )
+#
+# kmeans = KMeans(n_clusters=2, n_init = 10, max_iter = 300).fit(finance_features)
+# print(kmeans.labels_)
+# print(type(poi))
+#
+# pred = clf.predict(finance_features)
+# kmeans.cluster_centers_
 
-
+clf3 = KMeans(n_clusters=3)
+pred3 = clf3.fit_predict( finance_features )
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred2, finance_features, poi, mark_poi=False, name="clusters_n2.png", f1_name=feature_1, f2_name=feature_2)
+
+except NameError:
+    print("no predictions object named pred found, no clusters to plot")
+
+try:
+    Draw(pred3, finance_features, poi, mark_poi=False, name="clusters_n3.png", f1_name=feature_1, f2_name=feature_2)
+
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
