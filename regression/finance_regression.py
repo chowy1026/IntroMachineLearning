a621f02f88eae04818cafcb7548f03755293af03
chowy1026 <chowy1026@gmail.com>
2017-04-13 16:10:20 +0800

    linear regressions exercises

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -2,7 +2,7 @@
 
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,34 +10,60 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 
 import sys
 import pickle
+import pprint
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
+# with open("../final_project/final_project_dataset.pkl", "rb") as f:
+#     enron_data = pickle.load(f)
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
+
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
+
+# Bonus against salary
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+# Bonus against long_term_incentive
+# features_list = ["bonus", "long_term_incentive"]
+
+# sort_keys fix for python3
+sort_keys = '../tools/python2_lesson06_keys.pkl'
+
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = sort_keys)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
+
 
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn import datasets, linear_model
+reg = linear_model.LinearRegression()
+reg = reg.fit(feature_train, target_train)
+
+# print("predicted networth with age 38: ", reg.predict([28]))
+print("coefficient: ", reg.coef_)
+print("intercept: ", reg.intercept_)
+
+print("\n ########## Test on Test Dataset ########## \n")
+print("r-squared score", reg.score(feature_test, target_test))
+
+print("\n ########## Test on Training Dataset ########## \n")
+print("r-squared score", reg.score(feature_train, target_train))
 
 
 
@@ -48,9 +74,9 @@ test_color = "b"
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -64,6 +90,14 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="green")
+# print("predicted networth with age 38: ", reg.predict([28]))
+print("coefficient: ", reg.coef_)
+print("intercept: ", reg.intercept_)
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
