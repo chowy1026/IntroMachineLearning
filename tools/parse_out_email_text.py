c0d3fa582e24a3a80c9777f9ece39d88bbc82d8f
chowy1026 <chowy1026@gmail.com>
2017-04-14 22:16:54 +0800

    completed parseOutText function

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 from nltk.stem.snowball import SnowballStemmer
-import string
+import string, re
+
+from nltk.stem import *
+
+
 
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
@@ -11,7 +15,7 @@ def parseOutText(f):
         in the email (space-separated)
 
         example use case:
-        f = open("email_file_name.txt", "rb")
+        f = open("email_file_name.txt", "r")
         text = parseOutText(f)
 
         """
@@ -19,31 +23,37 @@ def parseOutText(f):
 
     f.seek(0)  ### go back to beginning of file (annoying)
     all_text = f.read()
+    print(all_text)
 
     ### split off metadata
     content = all_text.split("X-FileName:")
     words = ""
     if len(content) > 1:
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
-
+        # print(len(string.punctuation))
+        # text_string = content[1].translate(None, string.punctuation)
+        text_string = re.sub('[%s]' % re.escape(string.punctuation), '', content[1])
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
+
+        orig_words = text_string.split()
+        # print(len(orig_words))
+
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-
-
-
-
-
+        words = ""
+        stemmer = SnowballStemmer("english")
+        for w in orig_words:
+            words += " " + stemmer.stem(w)
+            # append(stemmer.stem(w)) if stemmer.stem(w) not in words
     return words
 
 
 
 def main():
-    ff = open("../text_learning/test_email.txt", "rb")
+    ff = open("../text_learning/test_email.txt", "r")
     text = parseOutText(ff)
     print(text)
 
